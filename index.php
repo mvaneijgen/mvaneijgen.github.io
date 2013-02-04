d46745da72cca9dd95bcd93995ef7162ea0b920f
mvaneijgen <mvaneijgen@gmail.com>
2013-02-04 13:16:53 +0100

    moved to PHP

---
+++ index.php
@@ -0,0 +1,53 @@
+<!doctype html>  
+<html lang="en">
+    
+    <head>
+        <meta charset="utf-8">
+        
+        <title>Jong creatief enzo</title>
+
+        <meta name="description" content="Wij laten zien dat je geen doorgewinterde ondernemer hoeft te zijn om een bedrijf op te richten. Je moet weten waar je voor staat en je hart in je werk stoppen.">
+        <meta name="author" content="M van Eijgen">
+        
+        <link href='http://fonts.googleapis.com/css?family=Open+Sans:400,600' rel='stylesheet' type='text/css'>
+        <script>
+        if (!("ontouchstart" in document.documentElement)) {
+            document.documentElement.className += " no-touch";
+            }
+        </script>
+        <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js"></script>
+        <link rel="stylesheet" href="assets/css/style.css">
+        <!-- Begin meny -->
+        <meta name="viewport" content="width=800, user-scalable=no">
+        <script src="assets/js/meny.js" type="text/javascript"></script>
+        <script src="assets/js/script.js" type="text/javascript"></script>
+        <!-- End meny -->
+                    
+    </head>
+    
+    <body>
+        <? include("meny.php"); ?>
+        <div class="contents">
+        <!-- Begin experiment -->
+            <aside>
+                <section>
+                    <div class="intro">
+                        <h1>Ontwerp &amp; technologie.</h1>
+                        <p>Jong creatief enzo is een groep jonge ondernemers die kennis delen omdaar samen beter van te worden.</p>
+                        <p>Alles wat wij kunnen doen is niet meer dan ons hart in ons werk stoppen <span class="data-icon">&#xe003;</span></p>
+                    </div>
+                    
+                    <div class="projecten">
+                        <h1>Projecten</h1>
+                        <a class="tumb" href="#"><img src="/assets/img/tumb/volvo_tumb.jpg" data-url="assets/img/volvoprints.jpg"></a>
+                        <a class="tumb" href="#"><img src="/assets/img/tumb/mag_tumb.jpg" data-url="assets/img/bosbode.jpg"></a>
+                        <a class="tumb" href="#"><img src="/assets/img/tumb/inholland_tumb.jpg" data-url="assets/img/inholland.jpg"></a>
+                        <a class="tumb" href="#"><img src="/assets/img/tumb/peek_tumb.jpg" data-url="assets/img/fabstagram.jpg"></a>
+                        <a class="tumb" href="#"><img src="/assets/img/tumb/fab_tumb.jpg" data-url="assets/img/fabstagram.jpg"></a>
+                    </div>
+                </section>
+            </aside>         
+        <!-- end experiment -->
+        </div>
+   </body>
+</html>
---
+++ meny.php
@@ -0,0 +1,48 @@
+        <div class="meny">
+            <h1 class="icon-logo" ><a href="/">Jong creatief enzo</a></h1>
+            <nav>
+                <ul>
+                    <li><a href="/blog">Blog</a></li>
+                    <li><a href="/contact">Contact</a></li>
+                </ul>
+                
+                <h2 class="icon-cube"><a href="/portfolio">Portfolio</a></h2>
+                <ul>
+                    <li><a href="/portfolio#grafisch">Grafisch ontwerp</a></li>
+                    <li><a href="/portfolio#web">Webdesign</a></li>
+                    <li><a href="/portfolio#app">Applicaties</a></li>
+                    <li><a href="/portfolio#font">Letter ontwerp</a></li>
+                </ul>
+    
+                <h2 class="icon-atom"><a href="/experimenten">Experimenten</a></h2>
+                <ul>
+                    <li><a href="/experimenten/paperjs">Paper.js</a></li>
+                    <li><a href="/experimenten/mask">Mask enzo</a></li>
+                    <li><a href="/experimenten/8bit">8Bit font</a></li>
+                </ul>
+            </nav>
+                
+            <section>
+                <h2 class="icon-chat">Contact</h2>
+                <ul>
+                    <li>Mitchel van Eijgen</li>
+                    <li class="space">&nbsp;</li>
+                    <li>Mitchel@jcenzo.nl</li>
+                    <li>06 38 63 27 35</li>
+                </ul>
+
+                <section class="social">
+                <span><a class="data-icon" href="http://www.facebook.com/jongcreatiefenzo" id="facebook" target="_blank" rel="no-follow">&#xe005;</a></span>
+                <span><a class="data-icon" href="http://www.twitter.com/jcenzo" id="twitter" target="_blank" rel="no-follow">&#xe006;</a></span>
+                <span><a class="data-icon" href="http://www.github.com/mvaneijgen" id="twitter" target="_blank" rel="no-follow">&#xe007;</a></span>
+                <span><a class="data-icon" href="http://dribbble.com/mvaneijgen" id="dribbble" target="_blank" rel="no-follow">&#xe004;</a></span>
+                </section>
+                <div class="contact">
+                    <a href="#"><button class="btn icon-cube no-touch">Contact een persoon</button></a>
+                    <a href="#"><button class="btn icon-twitter no-touch">Hulp nodig, send us a tweet</button></a>
+                </div>
+            </section>
+        </div>
+        
+
+        <div class="meny-arrow"></div>
