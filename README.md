a4dc70a8ce5a489d10325ae98d2aed813de908e8
mvaneijgen <mvaneijgen@gmail.com>
2014-08-15 12:20:44 +0200

    Fresh start: Jekyll & Meny merge.
    Branched of the old stuff and Installed a fresh Jekyll project and
    merged github.com/hakimel/Meny into the project.

--- README.md
+++
@@ -1,9 +0,0 @@
-jongcreatiefenzo
-================
-
-
-New design for http://jcenzo.nl
-
-all thanks to: Meny A three dimensional and space effecient menu. Meny works best in browsers with support for CSS 3D transforms, although it falls back on 2D animation for older browsers. Supports touch events for mobile devices.
-
-https://github.com/hakimel/Meny
--- form.php
+++
@@ -1,5 +0,0 @@
- <div class="contact">
-    <a href="#" class="contactLink" data-url="assets/img/bosbodeProject.jpg"><div class="btn icon-cube no-touch">Meld een project aan</div></a>
-    <a href="http://twitter.com/jcenzo" class="twitter" data-url="assets/img/twitter.jpg"><div class="btn icon-twitter no-touch">Hulp nodig, send us a tweet</div></a>
-</div>
-
